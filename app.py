0fbae55d392192028ae9f29641f9dbe65b4892e0
weirdaxe <92172452+weirdaxe@users.noreply.github.com>
2024-10-09 18:39:33 +0100

    Update app.py

--- app.py
+++ app.py
@@ -265,7 +265,7 @@ if map_tog:
             height=800,
             width = 1800
         )
-        fig_map.layout.updatemenus[0].buttons[0].args[1]['frame']['duration'] = 100
+        fig_map.layout.updatemenus[0].buttons[0].args[1]['frame']['duration'] = 150
         fig_map.layout.updatemenus[0].buttons[0].args[1]['transition']['duration'] = 50
         st.plotly_chart(fig_map)
 if not map_tog:
